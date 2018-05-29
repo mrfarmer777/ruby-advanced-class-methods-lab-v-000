2a73ce0def4757f4b9874a9cde243153db8545fd
Matt Farmer <mrfarmer777@gmail.com>
2018-05-29 22:12:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,18 @@ class Song
   @@all = []
 
   def self.all
-    @@all
+    @@all #returns the class variable all
   end
 
   def save
-    self.class.all << self
+    self.class.all << self #instance method that goes back to class variable all??
+  end
+
+  def self.create(name,artist_name)
+    song=self.class.new
+    song.name=name
+    song.artist_name.artist_name
+    song.save    
   end
 
 end
